3ac7f603dc8ec0eb38d1fda59ee1e819b7b57218
Anastasia <Nastya170204@yandex.ru>
2022-10-12 20:51:44 +0700

    Delete solver.hs

--- pack-1/solver.hs
+++
@@ -1,4 +0,0 @@
-solver :: Double -> Double -> Double -> (Double, Double)
-solver a b c | (d >= 0) = ((-b + sqrt d) / (2*a), (-b - sqrt d) / (2*a))
-             | otherwise = error "No solution"
-            where d = b * b - 4*a*c
