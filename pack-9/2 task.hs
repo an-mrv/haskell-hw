9ab2a0f6b79b34bbb0d030320114d55e86830877
Anastasia <Nastya170204@yandex.ru>
2023-04-01 16:24:09 +0700

    Changes

--- pack-9/2 task.hs
+++ pack-9/2 task.hs
@@ -1,23 +1,26 @@
 import Control.Monad.State
 -- state monad
 
-type RandState = [Int]
+type RandState = Int
+
+array :: [Int]
+array = [4, 3, 5, 2, 1, 6]
+
+rollDice :: [Int] -> Int -> Int
+rollDice arr n = arr !! (n-1) 
 
 game :: State RandState String
 game = do
-    rollDice <- get
-    let firstPlayerRes = head (rollDice) 
-    put (tail (rollDice) ++ [head (rollDice)])
-    rollDice <- get
-    let secondPlayerRes = head (rollDice)
-    put (tail (rollDice) ++ [head (rollDice)])
+    a <- get
+    let firstPlayerRes = rollDice array a
+    put (firstPlayerRes)
+    b <- get
+    let secondPlayerRes = rollDice array b
+    put (secondPlayerRes)
     if firstPlayerRes > secondPlayerRes
         then return("First wins")
         else return("Second wins")
 
 runGame :: String
 runGame = evalState game startSeed
-    where startSeed = [4, 3, 5, 2, 1, 6]
-
---if startSeed = [3, 4, 5, 2, 1, 6] then "Second wins"
---if startSeed = [4, 3, 5, 2, 1, 6] then "First wins"
+    where startSeed = 5
